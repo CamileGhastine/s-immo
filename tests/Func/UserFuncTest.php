94e4b926d9d688dab9fe8eae3a01c4176c8313c2
Djikine Djibril <d.djikine@fmcproduction.com>
2022-03-01 00:11:27 +0100

    add func test : user(get collections, post)

--- tests/Func/UserFuncTest.php
+++ tests/Func/UserFuncTest.php
@@ -2,41 +2,32 @@
 
 namespace App\Tests\Func;
 
-use phpDocumentor\Reflection\Types\This;
+use ApiPlatform\Core\Bridge\Symfony\Bundle\Test\ApiTestCase;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Faker\Factory;
+use Hautelook\AliceBundle\PhpUnit\RefreshDatabaseTrait;
 
-class UserFuncTest extends EndPoint
+class UserFuncTest extends ApiTestCase
 {
-    /**
-     * TODO
-     */
-    /*private $userData = ["email"=>"test@test.fr", "roles"=>["ROLE_USER"], "password"=>"password", "firstname"=>"firstname", "lastname"=>"lastname"];
-    //private $userData = '{"name": "test@test.fr", "password": "password"}';
+    use RefreshDatabaseTrait;
+
     public function testGetUsers()
     {
-        $response = $this->getQuery(Request::METHOD_GET, '/api/users');
-        self::assertEquals(Response::HTTP_OK, intval($response->getStatusCode()));
+        $client = self::createClient();
+        $response = $client->request(Request::METHOD_GET, '/api/users');
+        self::assertEquals(Response::HTTP_OK, $response->getStatusCode());
     }
 
     public function testPostUsers(): void
     {
-        $userDataJson = json_encode($this->userData);
-        //dd([$userDataJson]);
-        //private $userData = ["email"=>"test@test.fr", "roles"=>["ROLE_USER"], "password"=>"password", "firstname"=>"firstname", "lastname"=>"lastname"];
-        $response = $this->getQuery(Request::METHOD_POST,'/api/users',['json' => ['email' => "test@test.fr", "roles"=>["ROLE_USER"], "password"=>"password", "firstname"=>"firstname", "lastname"=>"lastname"]]);
-        $responseContent = $response->getContent();
-        //dd("status code", $responseContent);
-        $responseDecode = json_decode($responseContent);
-        //self::assertEquals(Response::HTTP_CREATED, $response->getStatusCode());
-        self::assertJson($response->getContent());
-        self::assertNotEmpty($responseDecode);
-    }
-
-    private function getData(): string
-    {
         $faker = Factory::create();
-        return sprintf($this->userData, $faker->email);
-    }*/
+        $data = ['json' => ['email' => $faker->safeEmail, "roles"=>["ROLE_USER"], "password"=>"password", "firstname"=>"firstname", "lastname"=>"lastname"]];
+
+        $client = self::createClient();
+        $response = $client->request(Request::METHOD_POST, '/api/users', $data);
+        $responseContent = $response->getContent();
+        self::assertNotEmpty(json_decode($responseContent));
+        self::assertEquals(Response::HTTP_CREATED, $response->getStatusCode());
+    }
 }
